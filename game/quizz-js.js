ca1a959004f6d50668ba915bf86eb351fbbdd8ac
amandacrvlh <amanda.ufc.dee@gmail.com>
2025-03-18 23:07:27 -0300

    Add files via upload

---
+++ game/quizz-js.js
@@ -0,0 +1,96 @@
+let perguntas = [
+
+{
+    titulo: 'Qual o nosso aniversário de namoro?',
+    alternativas: ['dia 19', 'dia 06', 'dia 07', 'dia 12'],
+    correta: 2
+},
+
+{
+    titulo: 'Qual o dia do meu aniversário?',
+    alternativas: ['29/02', '20/12', '09/01', '20/07'],
+    correta: 3  
+},
+
+{
+    titulo: 'Qual jogo eu ja fui profissional',
+    alternativas: ['Futsal', 'Free Fire', 'Xadrez', 'Just Dance'],
+    correta: 1
+},
+
+{
+    titulo: 'Qual meu time do coração?',
+    alternativas: ['Ceara', 'Fortaleza', 'Palmeiras', 'Flamengo'],
+    correta: 0
+},
+
+{
+    titulo: 'Pronta para a pergunta mais importante?!',
+    alternativas: ['Sim', 'Não', 'Talvez', '-'],
+    correta: 0
+},
+
+
+]
+
+let app = {
+    start: function(){
+        this.Atualpos = 0;
+        this.Totalpontos = 0;
+        this.Erros = 0;
+
+        let alts = document.querySelectorAll('.alternativa');
+        alts.forEach((element, index)=>{
+            element.addEventListener('click', ()=>{
+                this.checaResposta(index);
+            })
+        })
+        this.atualizaPontos();
+        app.mostraquestao(perguntas[this.Atualpos]);
+    },
+
+    mostraquestao: function(q){
+        this.qatual = q;
+        //mostrando titulo
+        let titleDiv = document.getElementById('titulo');
+        titleDiv.textContent = q.titulo;
+        //mostrando alternativas
+        let alts = document.querySelectorAll('.alternativa');
+        alts.forEach(function(element, index){
+            element.textContent = q.alternativas[index];
+        })
+    },
+
+    checaResposta: function(user){
+        if(this.qatual.correta == user){
+            console.log("Correto");
+            this.Totalpontos++;
+        }else{
+            console.log("Errado");
+            this.Erros++;
+        }
+        this.atualizaPontos();
+        this.Proximaperg();
+        this.mostraquestao(perguntas[this.Atualpos]);
+    },
+
+    Proximaperg: function(){
+        this.Atualpos++;
+        if(this.Atualpos == perguntas.length){
+            if(this.Totalpontos == 5){
+                location.href = "/duwdwm/duwdwm.html";
+            }else{
+                alert(`Tente novamente! Você errou um total de: ${this.Erros}`);
+                location.href = "/quizz/quizz.html";
+            }
+        }
+    },
+
+    atualizaPontos: function(){
+        let scoreDiv = document.getElementById('pontos');
+        if(this.Totalpontos >= 5)
+        scoreDiv.textContent = `Sua pontuação é: ${this.Totalpontos}`;
+    }
+
+}
+app.start();
